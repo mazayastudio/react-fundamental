c958d593c80b1538009bc44c371e8fc7cc03742f
sayudhalw <mazaya.studio@outlook.com>
2024-01-10 09:36:43 +0700

    Refactor list rendering in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-
-
 const list = [
 	{
 		title: "React",
@@ -31,7 +29,16 @@ function App() {
 
 			<ul>
 				{list.map(function (item) {
-					return <li key={item.objectID}>{item.title}</li>;
+					return (
+						<li key={item.objectID}>
+							<span>
+								<a href="{item.url}">{item.title}</a>
+							</span>
+							<span>{item.author}</span>
+							<span>{item.num_comments}</span>
+							<span>{item.points}</span>
+						</li>
+					);
 				})}
 			</ul>
 		</div>
