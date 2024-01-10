0cdcffe74a2bacc5696e47cef8a2dcc4d9995516
sayudha <mazaya.studio@outlook.com>
2024-01-10 08:33:13 +0700

    Refactor App component to use a function to get the title

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import * as React from "react";
 
-const welcome = {
-	greeting: "Hey",
-	title: "React",
-};
+function getTitle(title) {
+	return title;
+}
 
 function App() {
 	return (
 		<div>
-			<h1>
-				{welcome.greeting} {welcome.title}
-			</h1>
+			<h1>hello {getTitle("React")}</h1>
 
 			<label htmlFor="search">Search:</label>
 			<input type="text" id="search" />
