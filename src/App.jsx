81cda9a5c30e917ad46c2d5cb3ff6029071ebaf9
sayudha <mazaya.studio@outlook.com>
2024-01-10 18:11:30 +0700

    Refactor List component and add PropTypes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-const list = [
-	{
-		title: "React",
-		url: "https://reactjs.org/",
-		author: "Jordan Walke",
-		num_comments: 3,
-		points: 4,
-		objectID: 0,
-	},
-	{
-		title: "Redux",
-		url: "https://redux.js.org/",
-		author: "Dan Abramov, Andrew Clark",
-		num_comments: 2,
-		points: 5,
-		objectID: 1,
-	},
-];
-
+import PropTypes from 'prop-types';
 const App = () => {
+	const stories = [
+		{
+			title: "React",
+			url: "https://reactjs.org/",
+			author: "Jordan Walke",
+			num_comments: 3,
+			points: 4,
+			objectID: 0,
+		},
+		{
+			title: "Redux",
+			url: "https://redux.js.org/",
+			author: "Dan Abramov, Andrew Clark",
+			num_comments: 2,
+			points: 5,
+			objectID: 1,
+		},
+	];
 	return (
 		<div>
 			<h1>My Hacker Stories</h1>
@@ -26,28 +26,41 @@ const App = () => {
 
 			<hr />
 
-			<List />
+			<List list={stories} />
 		</div>
 	);
 };
 
-const List = () => {
-	return (
-		<ul>
-			{list.map((item) => {
-				return (
-					<li key={item.objectID}>
-						<span>
-							<a href="{item.url}">{item.title}</a>
-						</span>
-						<span>{item.author}</span>
-						<span>{item.num_comments}</span>
-						<span>{item.points}</span>
-					</li>
-				);
-			})}
-		</ul>
-	);
+const List = (props) => (
+	<ul>
+		{props.list.map((item) => (
+			<Item key={item.objectID} item={item} />
+		))}
+	</ul>
+);
+List.propTypes = {
+	list: PropTypes.array.isRequired,
+};
+
+
+const Item = (props) => (
+	<li>
+		<span>
+			<a href={props.item.url}>{props.item.title}</a>
+		</span>
+		<span>{props.item.author}</span>
+		<span>{props.item.num_comments}</span>
+		<span>{props.item.points}</span>
+	</li>
+);
+Item.propTypes = {
+	item: PropTypes.shape({
+		url: PropTypes.string.isRequired,
+		title: PropTypes.string.isRequired,
+		author: PropTypes.string.isRequired,
+		num_comments: PropTypes.number.isRequired,
+		points: PropTypes.number.isRequired,
+	}).isRequired,
 };
 
 const Search = () => {
