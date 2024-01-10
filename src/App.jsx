8c7e2a0ca269837551a9ff82e48d039d3343f142
sayudhalw <mazaya.studio@outlook.com>
2024-01-10 09:21:41 +0700

    Refactor App component to display a list of items

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-import * as React from "react";
 
-const title = "React";
+
+const list = [
+	{
+		title: "React",
+		url: "https://reactjs.org/",
+		author: "Jordan Walke",
+		num_comments: 3,
+		points: 4,
+		objectID: 0,
+	},
+	{
+		title: "Redux",
+		url: "https://redux.js.org/",
+		author: "Dan Abramov, Andrew Clark",
+		num_comments: 2,
+		points: 5,
+		objectID: 1,
+	},
+];
 
 function App() {
 	return (
 		<div>
-			<h1>Hello World</h1>
+			<h1>My Hacker Stories</h1>
 
 			<label htmlFor="search">Search:</label>
 			<input type="text" id="search" />
+
+			<hr />
+
+			<ul>
+				{list.map(function (item) {
+					return <li key={item.objectID}>{item.title}</li>;
+				})}
+			</ul>
 		</div>
 	);
 }
